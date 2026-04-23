31f2314e2e956cef8dbe9e399946bdd3e8534b54
ericMervinAS <eric.m@aftershoot.com>
2026-04-23 09:57:52 +0530

    Vercel fix part 4

--- api/index.mjs
+++ api/index.mjs
@@ -1,3 +1,9 @@
 import server from "../dist/server/server.js";
 
-export default (request) => server.fetch(request);
+export default async (request) => {
+  const host = request.headers.get("host") ?? "localhost";
+  const proto = request.headers.get("x-forwarded-proto") ?? "https";
+  const absoluteUrl = new URL(request.url, `${proto}://${host}`);
+  const absoluteRequest = new Request(absoluteUrl, request);
+  return server.fetch(absoluteRequest);
+};
